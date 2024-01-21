674f99544e419e4f82d07653d6e35a8e495577d1
Roshan <roshan.2201021cs@iiitbh.ac.in>
2024-01-21 14:08:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+const cors = require("cors");
 
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methords: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 connectDB();
 
